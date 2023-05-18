e85ae6815a3db5bedd1fc87b5fe5667bd635273a
basqueoro <basqueroto.felipe125@gmail.com>
2023-05-18 17:01:59 -0300

    finish for js

--- script/script.js
+++ script/script.js
@@ -2,63 +2,116 @@ var form = document.getElementById('form')
 var container = document.getElementById('container')
 var inp = document.getElementsByClassName('nota')
 var pai = document.getElementById('pai')
+var txt = document.getElementById('texto')
+var contagem = 0
+var position = []
 
 function gerar (btn) {
     var pai = btn.parentElement
+    var n = btn.dataset.indice
+    position.push(parseFloat(n))
+    console.log(position)
     pai.innerHTML = ""
     pai.innerHTML = `
-               <input type="text" class="peso" name="peso" required>
-               <button onclick="apagar(this)">apagar</button>
+               <input type="text" class="pesoB" name="peso" required>
+               <button data-indice="`+ n +`" onclick="apagar(this)">apagar</button>
         `
 }
 
 function apagar (btn) {
     var pai = btn.parentElement
+    var n = btn.dataset.indice
+    if (position.length < 2) {
+        position.pop()
+    }else {
+        for (var i = 0; i < position.length; i++) {
+            if (position[i] == n) {
+                position.splice(i, 1)
+            }
+        }
+    } 
+    console.log(position)
     pai.innerHTML = ""
     pai.innerHTML = `
-    <button class="bnt-peso" onclick="gerar(this)">gerar</button>           
+    <button data-indice="`+ n +`" class="bnt-peso" onclick="gerar(this)">gerar</button>           
       `
 }
 
 function add () {
+    contagem += 1
+    console.log(contagem)
     container.innerHTML += `
     <div class="contain-name">
         <div class="inpt">
             <label for="nota" class="label">Notas</label>
-            <input type=text name="nota1" class="nota" required="" placeholder="=digite a nota">
+            <input type=text name="nota1" class="nota">
         </div>
         <div class="peso">
-            <button class="bnt-peso" onclick="gerar(this)">gerar</button>   
+            <button data-indice="`+ contagem +`" class="bnt-peso" onclick="gerar(this)">gerar</button>   
         </div> 
     </div>
     `
 }
 
 function tra () {
+    contagem = contagem - 1 
+    position.pop()  
     container.removeChild(container.lastElementChild) 
 }
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-
     var inputs = document.querySelectorAll(".nota")
-    var peso = document.querySelectorAll(".peso")
+    var peso = document.querySelectorAll(".pesoB")
     var aInp = []
     var aPeso = []
+    var soma = 0
+    var somanote = 0
+    var final = 0
 
-    for (let i = 0; i < inputs.length; i++) {
-        aInp.push(inputs[i].value)
+    for (var i = 0; i < inputs.length; i++) {
+        aInp.push(parseFloat(inputs[i].value))
     }
 
-    for (let i = 0; i < peso.length; i++) {
-        aPeso.push(inputs[i].value)
+    for (var i = 0; i < peso.length; i++) {
+        aPeso.push(parseFloat(peso[i].value))
+    }
+    console.log(position)
+    if (aPeso.length > 0) {
+        for(var i = 0; i < position.length; i++){
+            aInp[position[i]] = aPeso[i] * aInp[position[i]]
+        }
+        for(var i = 0; i < aPeso.length; i++) {
+            soma += aPeso[i]
+        }
+        for(var i = 0; i < aInp.length; i++) {
+            somanote += aInp[i]
+        }
+
+        console.log(aPeso)
+        console.log(aInp)
+
+        soma += aInp.length - aPeso.length
+        final = somanote / soma
+        console.log(final)
+    }
+    else {
+        for(var i = 0; i < aInp.length; i++) {
+            somanote += aInp[i]
+        }
+        final = somanote / aInp.length
+        console.log(final)
     }
 
-    
-
-
-    console.log(aInp)
-    console.log(aPeso)
+    if (final >= 7) {
+        texto.innerHTML = "o aluno passou com a nota de " + final.toFixed(1)
+    }
+    else if (final >= 5) {
+        texto.innerHTML = "o aluno ficou de exame com a nota de " + final.toFixed(1)
+    }
+    else {
+        texto.innerHTML = "o aluno foi reprovado " + final.toFixed(1)
+    }
 })
 
 
